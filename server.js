443c17219cbfa9decd56bb655e99df0ebe1e437f
Daniel Grummon <grummon1@hotmail.com>
2020-10-25 07:48:41 -0400

    heroku deploy

--- server.js
+++ server.js
@@ -2,11 +2,12 @@
 const path = require("path");
 const express = require("express");
 const app = express();
-app.use(express.static(__dirname + '/pwingei'));
 
-app.get('/*', function(req,res){
-res.sendFile(path.join(__dirname, 'pwingei', 'index.html'))
-});
+app.use(express.static('./dist/pwingeiFrontendLite'));
 
+app.get('/*', function(req, res) {
+    res.sendFile('index.html', {root: 'dist/pwingeiFrontendLite/'}
+  );
+  });
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
