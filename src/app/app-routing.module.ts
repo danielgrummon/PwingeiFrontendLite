0492d02d7f30a2c5ee7331580c269d442d59c4b7
Daniel Grummon <grummon1@hotmail.com>
2020-10-25 16:17:51 -0400

    added layout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,16 +2,21 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import {ThelistComponent} from './thelist/thelist.component';
+import { HomeComponent } from './home/home.component';
+import { MinimalistComponent } from './minimalist/minimalist.component';
+import { MainComponent } from './main/main.component';
+
 
 const routes: Routes = [
-//  { path: '', component: ThelistComponent },
-  { path: 'TheList', component: ThelistComponent }
+ // { path: '', pathMatch: 'full', redirectTo: 'home' },
+  { path: 'home', component: MainComponent },
+  { path: 'list', component: ThelistComponent },
+  { path: 'minimalist', component: MinimalistComponent }
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule],
-  declarations: []
+  exports: [RouterModule]
 })
 
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,25 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { ThelistComponent } from './thelist/thelist.component';
 import { AppRoutingModule } from './app-routing.module';
+import { HomeComponent } from './home/home.component';
+import { MinimalistComponent } from './minimalist/minimalist.component';
+import { HeaderComponent } from './header/header.component';
+import { NavComponent } from './nav/nav.component';
+import { MainComponent } from './main/main.component';
+import { AsideComponent } from './aside/aside.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ThelistComponent
+    ThelistComponent,
+    HomeComponent,
+    MinimalistComponent,
+    HeaderComponent,
+    NavComponent,
+    MainComponent,
+    AsideComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
