ab120702b3670a85c0403dcee45a57d4ff1fa8cc
Daniel Grummon <grummon1@hotmail.com>
2020-10-25 00:17:32 -0400

    heroku deploy

--- server.js
+++ server.js
@@ -2,7 +2,10 @@ const express = require('express');
 const path = require('path');
 const ngApp = express();
 ngApp.use(express.static('./dist/pwingei-frontend-lite'));
-ngApp.get('/*', function (request, response) {
-    response.sendFile(path.join(__dirname, '/dist/pwingei-frontend-lite/index.html'));
-});
+
+app.get('*', function (req, res) {
+    const index = path.join(__dirname, 'src', 'index.html');
+    res.sendFile(index);
+  });
+
 ngApp.listen(process.env.PORT || 8080);
