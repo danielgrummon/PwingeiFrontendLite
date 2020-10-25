751a96a1693ce29276d7f9d76b4f85e97e31d554
Daniel Grummon <grummon1@hotmail.com>
2020-10-24 22:18:50 -0400

    prepare for heroku deployment

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.static('./dist/pwingei-frontend-lite'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/pwingei-frontend-lite/'}),
+    res.sendFile('index.html', {root: 'dist/pwingei-frontend-lite/src/'}),
 );
 
 app.listen(process.env.PORT || 8080);
