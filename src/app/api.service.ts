d1f8bcd8fb270e994e764a04f579785919066d54
Daniel Grummon <grummon1@hotmail.com>
2020-12-15 20:44:15 -0500

    improved service

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import {Observable} from 'rxjs';
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
 import { Phenotype } from './thelist/thelist.model';
+import { catchError, tap } from 'rxjs/operators';
 
 
 @Injectable({
@@ -9,7 +10,8 @@ import { Phenotype } from './thelist/thelist.model';
 })
 export class ApiService {
 
-  constructor(private httpClient:HttpClient) { }
+  private phenotypeListUrl = 'https://pwingei-backend.herokuapp.com/app/rest/wingei/pwingei';
+  constructor(private httpClient: HttpClient) { }
 
   options = {
     headers: new HttpHeaders({
@@ -17,9 +19,24 @@ export class ApiService {
       'Accept': 'application/json'
     })
   };
-  
-  getPhenotypeList(): Observable<Phenotype[]>{
-    console.log("service retrieving list");  
-    return this.httpClient.get<Phenotype[]>("https://pwingei-backend.herokuapp.com/app/rest/wingei/pwingei");
-     }
+
+  getPhenotypeList(): Observable<Phenotype[]> {
+    console.log("service retrieving list");
+    return this.httpClient.get<Phenotype[]>(this.phenotypeListUrl)
+      .pipe(
+        tap(data => console.log(JSON.stringify(data))),
+        catchError(this.handleError)
+      );
+  }
+
+  private handleError(err: HttpErrorResponse) {
+    let errorMessage = '';
+    if (err.error instanceof ErrorEvent) {
+      errorMessage = `An error occurred: ${err.error.message}`;
+    } else {
+      errorMessage = `Server returned code: ${err.status}, error message is: ${err.message}`;
+    }
+    console.error(errorMessage);
+    return throwError(errorMessage);
+  }
 }
